392658e789c34b5e614e62e87f2fd41a5413a5e2
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-05-04 00:39:02 +0100

    sum up a lisst

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -6,4 +6,5 @@ input_list of floats as argument and returns their sum as a float.
 from typing import List
 
 def sum_list(input_list: List[float]) -> float:
-    """ l"""
+    """ Returns the sum of a list """
+    return sum()
