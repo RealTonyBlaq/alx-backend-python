4ee3bb36e2d3a5e24f8a9380519286c94f32886a
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-05-09 00:41:32 +0100

    tasks

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,2 +1,6 @@
 #!/usr/bin/env python3
-""" task_wait_random that takes an integer max_delay and returns a asyncio.Task """
+"""
+task_wait_random that takes an integer max_delay
+and returns a asyncio.Task
+"""
+
