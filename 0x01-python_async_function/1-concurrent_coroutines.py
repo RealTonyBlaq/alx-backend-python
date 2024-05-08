bf30585338024c5b248ffe040c8bee4fd08d235a
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-05-08 23:47:44 +0100

    concurrent routines

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+"""
+An async routine called wait_n that takes in 2 int arguments(in this order): n and max_delay. You will spawn wait_random n times with the specified max_delay.
+"""
