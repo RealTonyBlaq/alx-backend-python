359280b996a06a4fa528914cbd307b5b5b384a9e
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-05-09 00:17:03 +0100

    mesures runtime

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+"""
+Creates a measure_time function with integers n and max_delay as arguments that measures the total execution time for wait_n(n, max_delay), and returns total_time / n. Your function should return a float.
+"""
