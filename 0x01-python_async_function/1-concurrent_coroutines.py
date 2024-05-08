92c811e2a5773ebcfa4ad0f084d99808180b67c3
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-05-08 23:57:09 +0100

    concurrent routines

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -14,4 +14,5 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """ Spawns wait_random n times and saves the delay to a list """
-    for 
+    delays = [asyncio.run(wait_random(max_delay)) for _ in range(n)]
+    return sorted(delays)
