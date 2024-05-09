671d5b4b13027d02fdacf52849c8762098eeb09b
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-05-09 01:13:22 +0100

    tasks

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -8,7 +8,7 @@ is being called.
 import asyncio
 
 
-task_wait_random = __import__('')
+task_wait_random = __import__('3-tasks').task
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """ Spawns wait_random n times and saves the delay to a list """
