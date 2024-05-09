25cd38d98218f728fa97b81aeca219bc9c454c4b
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-05-09 01:08:55 +0100

    tasks

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""
+Take the code from wait_n and alter it into a new function task_wait_n.
+The code is nearly identical to wait_n except task_wait_random
+is being called.
+"""
