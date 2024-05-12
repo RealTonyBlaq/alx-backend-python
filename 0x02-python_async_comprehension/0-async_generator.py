ae31976a30c9afdcb331b6b64738bc11bfda2cf8
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-05-12 13:21:26 +0100

    generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -10,8 +10,7 @@ import random
 from typing import Generator
 
 
-async def async_generator() -> Generator[int]:
+async def async_generator() -> Generator[int, None, None]:
     """ Coroutine loops 10 times, yields a random number each time """
     async for i in range(10):
         yield i
-        
