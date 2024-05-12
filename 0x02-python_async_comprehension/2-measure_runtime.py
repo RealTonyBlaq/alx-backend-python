10cc4c1af9256a793cf150f00472403a7eed529f
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-05-12 14:07:33 +0100

    measure runtime

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """
-measure_runtime coroutine that will execute async_comprehension four times in parallel using asyncio.gather.
-
-measure_runtime should measure the total runtime and return it."""
+measure_runtime - coroutine that will execute async_comprehension
+four times in parallel using asyncio.gather.
+measure_runtime should measure the total runtime and return it.
+"""
