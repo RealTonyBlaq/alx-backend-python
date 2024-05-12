d552f272fe7a90c190b9a2cc2bb53fb2310c152c
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-05-12 12:54:10 +0100

    generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,3 +4,6 @@ async_generator - takes no arguments.
 The coroutine will loop 10 times, each time asynchronously wait 1 second,
 then yield a random number between 0 and 10. Use the random module
 """
+
+import random
+
